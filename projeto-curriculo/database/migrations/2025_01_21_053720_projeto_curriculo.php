0f9203f8bb2490ed3b47254ad98812ae4719036a
TheV <djardim322@gmail.com>
2025-01-21 03:05:25 -0300

    fix: Observações podendo ser nulo agora.

--- projeto-curriculo/database/migrations/2025_01_21_053720_projeto_curriculo.php
+++ projeto-curriculo/database/migrations/2025_01_21_053720_projeto_curriculo.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use phpDocumentor\Reflection\Types\Nullable;
 
 return new class extends Migration
 {
@@ -18,7 +19,7 @@ return new class extends Migration
             $table->string('telefone');
             $table->string('cargo_desejado');
             $table->string('escolaridade');
-            $table->text('observacoes');
+            $table->text('observacoes')->nullable();
             $table->string('arquivo');
             $table->string('ip');
             $table->timestamps();
